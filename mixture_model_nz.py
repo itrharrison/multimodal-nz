8ce64acf75e4057e40a9d51033264ca530ec1a1f
Ian Harrison <itrharrison@gmail.com>
2019-07-04 11:38:12 +0100

    simple gaussian mixtures

--- mixture_model_nz.py
+++ mixture_model_nz.py
@@ -1,4 +1,9 @@
 import numpy as np
+from scipy.integrate import cumtrapz
+from scipy.stats import norm, gamma
+
+model_dict = {'gaussian' : norm.pdf,
+              'gamma' : gamma.pdf}
 
 class mixture_nz:
 
@@ -6,22 +11,40 @@ class mixture_nz:
   # weights, means, variances
 
   def __init__(self, weights, means, variances, models='gaussian'):
-    self.n_components = len(weights)
-    self.weights = np.asarray(weights)
-    self.means = np.asarray(means)
-    self.variances = np.asarray(variances)
-    if isiterable(models):
+    self.n_components = np.size(weights)
+    self.weights = np.atleast_1d(weights)
+    self.means = np.atleast_1d(means)
+    self.variances = np.atleast_1d(variances)
+    
+    if np.size(models) > 1:
       self.models = np.asarray(models)
     else:
       self.models = np.asarray([models]*self.n_components)
 
-    def get_mean_z(self):
-      mean_z = np.sum(self.weights*self.means)
+  def mean_z(self):
+    mean_z = np.sum(self.weights*self.means)
+    return mean_z
 
-      return mean_z
+  def gridded_nz(self, zmin, zmax, npix=512, normed=False):
+
+    nz = np.zeros(npix)
+    z = np.linspace(zmin, zmax, npix)
+
+    for im,model in enumerate(self.models):
+      nz += self.weights[im]*model_dict[model](z, self.means[im], self.variances[im])
+
+    if normed:
+      nz = nz / cumtrapz(nz, z)[-1]
+
+    return nz
 
 
 if __name__=='__main__':
 
-  single_mode = mixture_nz(1., 1., 0.5)
-  double_mode = mixture_nz([1.0, 0.5], [1.0, 0.1], [0.5, 0.5])
+  m1 = mixture_nz(1., 1., 0.5)
+  m2 = mixture_nz(1., 0.1, 0.5)
+  m12 = mixture_nz([1.0, 0.5], [1.0, 0.1], [0.5, 0.5])
+
+  nz1 = m1.gridded_nz(0,2)
+  nz2 = m2.gridded_nz(0,2)
+  nz12 = m12.gridded_nz(0,2)
